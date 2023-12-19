bd3007868aec15b5c1bf94486083e2f6fa2cdf3f
kyle <363130890@qq.com>
2023-12-19 20:41:46 +0800

    优化表现

--- Assets/FrameLine/TestAudio.cs
+++ Assets/FrameLine/TestAudio.cs
@@ -1,6 +1,7 @@
 using FrameLine;
 
 [TypeIdentify("024a7849-54b1-41f8-b037-2022d56600a0")]
+[DisplayName("音效"), FrameActionColor("#DEA32C")]
 public class TestAudio : IFrameEvent
 {
 }
--- Assets/FrameLine/TestEffect.cs
+++ Assets/FrameLine/TestEffect.cs
@@ -1,5 +1,6 @@
 using FrameLine;
 [TypeIdentify("68121115-ea12-4ba1-9a83-bb0b9d32beff")]
+[DisplayName("特效"), FrameActionColor("#F58F98")]
 public class TestEffect : IFrameClip
 {
 }

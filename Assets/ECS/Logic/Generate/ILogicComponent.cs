d36c0511df45700b9b534d8df04f7e266744a8a9
kyle <363130890@qq.com>
2024-01-02 12:01:21 +0800

    修复生成错误

--- Assets/ECS/Logic/Generate/ILogicComponent.cs
+++ Assets/ECS/Logic/Generate/ILogicComponent.cs
@@ -1,13 +1,3 @@
 ﻿
-public interface ILogicComponent : ILogic, ECSLite.IComponent
-{
-}
 
-public interface ILogicUniqueComponent : ILogic, ECSLite.IUniqueComponent
-{
-}
-
-public interface ILogicStaticComponent : ILogic, ECSLite.IStaticComponent
-{
-}
 
--- Assets/ECS/Logic/Generate/LogicContext.cs
+++ Assets/ECS/Logic/Generate/LogicContext.cs
@@ -1,6 +1,16 @@
-﻿using ECSLite;
+﻿public interface ILogicComponent : ILogic, ECSLite.IComponent
+{
+}
 
-public class LogicContext : ContextT<ILogic>
+public interface ILogicUniqueComponent : ILogic, ECSLite.IUniqueComponent
+{
+}
+
+public interface ILogicStaticComponent : ILogic, ECSLite.IStaticComponent
+{
+}
+
+public class LogicContext : ECSLite.ContextT<ILogic>
 {
     public LogicContext(int componentCount, int uniqueCount, int staticComponentCount) : 
         base(componentCount, uniqueCount, staticComponentCount)

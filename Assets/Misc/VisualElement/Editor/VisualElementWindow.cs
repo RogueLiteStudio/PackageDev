48727a2d8c8d3a4ff23a1e537d174ad0cf7bbebb
kyle <363130890@qq.com>
2024-06-28 16:03:46 +0800

    修复资源选择工具

--- Assets/Misc/VisualElement/Editor/VisualElementWindow.cs
+++ Assets/Misc/VisualElement/Editor/VisualElementWindow.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,53 +12,12 @@ public class VisualElementWindow : EditorWindow
     }
     public void CreateGUI()
     {
-        var rootScroll = new VisualElement();
-        rootScroll.style.flexDirection = FlexDirection.Column;
-        rootScroll.style.backgroundColor = Color.gray;
-        for (int i=0; i<5; ++i)
-        {
-            var child = new VisualElement();
-            child.style.backgroundColor = Color.green;
-            child.style.minHeight = 20;
-            child.style.marginTop = 10;
-            rootScroll.Add(child);
-            int childCount = i / 2;
-            var container = new VisualElement();
-            container.style.backgroundColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-            container.style.top = 1;
-            container.style.bottom = 1;
-            container.style.left = 0;
-            container.style.right = 0;
-            container.style.flexDirection = FlexDirection.Column;
-            child.Add(container);
-            for (int j=0; j<childCount; ++j)
-            {
-                var c = new VisualElement();
-                c.style.backgroundColor = Color.blue;
-                c.style.height = 10;
-                c.style.marginTop = 5;
-                container.Add(c);
-            }
-        }
-        var btn = new Button();
-        btn.text = "Fold";
-        btn.clicked += () =>
-        {
-            var children = rootScroll.Children();
-            foreach (var child in children)
-            {
-                if (child.childCount == 0)
-                    continue;
-                var c = child.ElementAt(0);
-                float height = child.contentRect.height;
-                bool isVisible = c.style.display == DisplayStyle.Flex;
-                c.style.display = isVisible ? DisplayStyle.None : DisplayStyle.Flex;
-                Debug.Log($"{height} --> {child.contentRect.height}");
-            }
-        };
+        var assetField = new NamedAsset.Editor.NamedAssetField("test");
+        assetField.AssetType = typeof(GameObject);
+        rootVisualElement.Add(assetField);
 
-        rootScroll.Add(btn);
-        rootVisualElement.Add(rootScroll);
-        rootScroll.StretchToParentSize();
+        var template = new ObjectField("Template");
+        template.objectType = typeof(GameObject);
+        rootVisualElement.Add(template);
     }
 }
